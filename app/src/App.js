8d5e05f0655dd43e5288ebde9c81f7da7bec701c
Adam Nolan <Adam@Adams-MBP.fios-router.home>
2020-09-08 17:44:15 -0400

    make react

---
+++ app/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+        <p>
+          init
+        </p>
+        <a
+          className="lonk"
+          href="github.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Turn back now as I work out what to put here
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
